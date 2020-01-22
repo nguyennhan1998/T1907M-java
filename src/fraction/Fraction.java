7833ea322eac750cc4be15cd293fdf54762dae5c
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2020-01-22 16:35:42 +0700

    make it better

--- src/fraction/Fraction.java
+++ src/fraction/Fraction.java
@@ -1,57 +1,65 @@
 package fraction;
 
 public class Fraction {
-   int a;
-   int b;
+    int a;
+    int b;
 
 
+    public Fraction() {
 
-public Fraction(){
+    }
 
-   }
-   public Fraction(int a,int b){
-       this.a=a;
-       this.b=b;
-   }
-   public void printFraction(){
-       System.out.println("the fraction is "+a+ "/"+b);
-   }
-   public int Ucln(int a,int b){
-       if(a%b!=0){
-           return Ucln(b,a%b);
+    public Fraction(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
-       }else{
-           return b;
-       }
-   }
-   public void rutGon(){
-       int ucln=Ucln(a,b);
-       a=a/ucln;
-       b=b/ucln;
-       if(b!=1){
-           System.out.println("the fraction is : "+a+ "/"+b);
-       }else{
-           System.out.println("the fraction is : "+a);
-       }
-   }
-    public void nghichDao(){
-        if(a!=0){
-            System.out.println("so nghich dao la: "+b+"/"+a);
-        }else{
+    public void printFraction() {
+        System.out.println("the fraction is " + a + "/" + b);
+    }
+
+    public int Ucln(int a, int b) {
+        if (a % b != 0) {
+            return Ucln(b, a % b);
+
+        } else {
+            return b;
+        }
+    }
+
+    public void rutGon() {
+        int ucln = Ucln(a, b);
+        a = a / ucln;
+        b = b / ucln;
+        if (b != 1) {
+            System.out.println("the fraction is : " + a + "/" + b);
+        } else {
+            System.out.println("the fraction is : " + a);
+        }
+    }
+
+    public void nghichDao() {
+        if (a != 0) {
+            System.out.println("so nghich dao la: " + b + "/" + a);
+        } else {
             System.out.println("khong co so nghich  dao");
         }
     }
-    public int add(){
-       return a+b;
+
+    public int add() {
+        return a + b;
     }
-    public int sub(){
-       return a-b;
+
+    public int sub() {
+        return a - b;
     }
-    public int mul(){
-       return a*b;
+
+    public int mul() {
+        return a * b;
     }
-    public double div(){
-       return (double) (a/b);
+
+    public double div() {
+        return (double) (a / b);
     }
 
     public int getA() {
