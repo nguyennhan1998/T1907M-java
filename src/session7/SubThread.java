71c454e98abb85dd61a11a016015bfc3f7235094
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2020-03-11 12:44:14 +0700

    lab_7

--- src/session7/SubThread.java
+++ src/session7/SubThread.java
@@ -3,16 +3,13 @@ package session7;
 public class SubThread extends Thread {
     @Override
     public void run() {
-        for(int i=10;i>0;i--){
+        for (int i = 10; i > 0; i--) {
             System.out.println(i);
-            try{
+            try {
                 Thread.sleep(1000);
 
-            }catch (Exception e){
-        }
-
-
-
+            } catch (Exception e) {
+            }
         }
         System.out.println("happy new year");
     }
