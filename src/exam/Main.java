2ee8c4a2f59f5992964c7e6a77e9c62ba16fe5ad
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2020-03-13 11:03:07 +0700

    make it better

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -6,45 +6,48 @@ public class Main {
     public static void main(String[] args) {
         AddressBook myPhone = new AddressBook();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter one action");
         int a;
-        a = sc.nextInt();
-        sc.nextLine();
+        do{
+            System.out.println("Enter one action");
+            a = sc.nextInt();
+            sc.nextLine();
 
 
-        switch (a) {
+            switch (a) {
 
-            case 1:
-                System.out.println("nhap vao so dien thoai");
-                String name = sc.nextLine();
-                System.out.println("nhap vao dia chi cong ty");
-                String company = sc.nextLine();
-                System.out.println("nhap vao dia chi email");
-                String email = sc.nextLine();
-                System.out.println("nhap vao so dien thoai");
-                String phone = sc.nextLine();
-                myPhone.addContact(name, company, email, phone);
-                break;
-            case 2:
-                System.out.println("nhap vao ten");
-                String sName = sc.nextLine();
+                case 1:
+                    System.out.println("nhap vao so dien thoai");
+                    String name = sc.nextLine();
+                    System.out.println("nhap vao dia chi cong ty");
+                    String company = sc.nextLine();
+                    System.out.println("nhap vao dia chi email");
+                    String email = sc.nextLine();
+                    System.out.println("nhap vao so dien thoai");
+                    String phone = sc.nextLine();
+                    myPhone.addContact(name, company, email, phone);
+                    break;
+                case 2:
+                    System.out.println("nhap vao ten");
+                    String sName = sc.nextLine();
 
-                Contact t = myPhone.searchContact(sName);
-                if (t == null) {
-                    System.out.println("khong tim thay ten");
-                } else {
-                    System.out.println("contact ban vua tim la: ");
-                    System.out.println("name " + t.name + ", " + t.company + ", " + t.email + ", " + t.phone);
-                }
-                break;
-            case 3:
-                myPhone.displayContact();
-                break;
-            case 4:
-                System.out.println("end the task");
-                break;
+                    Contact t = myPhone.searchContact(sName);
+                    if (t == null) {
+                        System.out.println("khong tim thay ten");
+                    } else {
+                        System.out.println("contact ban vua tim la: ");
+                        System.out.println("name " + t.getName() + ", " + t.getCompany() + ", " + t.getEmail() + ", " + t.getPhone());
+                    }
+                    break;
+                case 3:
+                    myPhone.displayContact();
+                    break;
+                case 4:
+                    System.out.println("end the task");
+                    break;
 
-        }
+            }
+
+        }while(a!=4);
 
 
     }
